32445740f3140a11488298bc7e94434405c83512
Alex Pieschel <apieschel@gmail.com>
2022-07-23 21:07:10 -0500

    add navigation menu styling

--- footer.php
+++ footer.php
@@ -9,6 +9,29 @@
 					$(".frankie").addClass("animated fadeInRight");
 				}
 			});
+
+			let parentMenuItems = Array.from( document.querySelectorAll( "nav ul li ul" ) );
+
+			parentMenuItems.forEach((element) => {
+				let menuButton = element.parentElement.querySelector("button");
+				let menuLastChild = element.querySelector("li:last-child > a");
+
+				if( menuButton ) {
+					menuButton.addEventListener("focusin", function () {
+						menuButton.setAttribute("aria-expanded", true);
+					} );
+
+					menuLastChild.addEventListener("blur", function () {
+						menuButton.setAttribute("aria-expanded", false);
+					} );
+
+					menuButton.addEventListener("mousedown", function () {
+						let menuState = menuButton.getAttribute("aria-expanded");
+						if (menuState === "false") { menuButton.setAttribute("aria-expanded", true); }
+						else { menuButton.setAttribute("aria-expanded", false);}
+					} );
+				}
+			} );
 		</script>
 	</body>
 </html>
--- header.php
+++ header.php
@@ -1,21 +1,23 @@
 <?php ob_start(); ?>
 
 <header>
-	<ul>
-		<li><a href="/web">Sites</a></li>
-		<li>
-			<a href="#">Projects</a>
-			<ul>
-				<li><a class="dropdown-item" href="/web/javascript-calculator">Samsung Galaxy S6 Calculator</a></li>
-				<li><a class="dropdown-item" href="/web/twitch-api">Twitch API</a></li>
-				<li><a class="dropdown-item" href="/web/tic-tac-toe">Tic Tac Toe</a></li>
-				<li><a class="dropdown-item" href="/web/simon">Simon</a></li>
-				<li><a class="dropdown-item" href="/web/pomodoro">Pomodoro</a></li>
-				<li><a class="dropdown-item" href="/web/gitfeed">Gitfeed</a></li>
-			</ul>
-		</li>	
-		<li><a href="/web/about">About</a></li>							
-	</ul>
+	<nav>
+		<ul>
+			<li><a href="/web">Sites</a></li>
+			<li>
+				<button aria-expanded="false">Projects</button>
+				<ul>
+					<li><a href="/web/javascript-calculator">Samsung Galaxy S6 Calculator</a></li>
+					<li><a href="/web/twitch-api">Twitch API</a></li>
+					<li><a href="/web/tic-tac-toe">Tic Tac Toe</a></li>
+					<li><a href="/web/simon">Simon</a></li>
+					<li><a href="/web/pomodoro">Pomodoro</a></li>
+					<li><a href="/web/gitfeed">Gitfeed</a></li>
+				</ul>
+			</li>	
+			<li><a href="/web/about">About</a></li>							
+		</ul>
+	</nav>
 </header>
 
 <?php 
