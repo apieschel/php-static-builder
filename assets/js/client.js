53a5e19c9b23c3d218fe0a18ee4a27c94a043bbb
Alex Pieschel <apieschel@gmail.com>
2022-07-26 15:46:02 -0500

    delete unnecessary http request

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,38 +1,30 @@
 document.addEventListener( 'DOMContentLoaded', () => {
-	const http = new XMLHttpRequest();
-	const url = '/music';
-	http.open( 'GET', url );
-	http.send();
-	http.onreadystatechange = function() {
-		if ( this.readyState == 4 ) {
-			$.ajax( {
-				url: '/music',
-				type: 'GET',
-				data: $( '#currentFiles' ).serialize(),
-				success: function( data ) {
-					if( data ) {
-						let jsonData = JSON.parse( data );
-						if ( jsonData.files && jsonData.files.length ) {
-							let select = document.createElement( 'select' );
-							select.required = 'true';
-							select.name = 'directory';
+	$.ajax( {
+		url: '/music',
+		type: 'GET',
+		data: $( '#currentFiles' ).serialize(),
+		success: function( data ) {
+			if( data ) {
+				let jsonData = JSON.parse( data );
+				if ( jsonData.files && jsonData.files.length ) {
+					let select = document.createElement( 'select' );
+					select.required = 'true';
+					select.name = 'directory';
 
-							for( let i = 0; i < jsonData.files.length; i++ ) {
-								let option = document.createElement( 'option' );
-								option.value = jsonData.files[i];
-								option.innerText = jsonData.files[i];
-								select.append( option );
-							}
-
-							document.querySelector( '#wavContainer' ).append( select );
-							document.querySelector( '#currentFiles' ).className = '';
-							document.querySelector( '#clearDirectory' ).className = '';
-						}
+					for( let i = 0; i < jsonData.files.length; i++ ) {
+						let option = document.createElement( 'option' );
+						option.value = jsonData.files[i];
+						option.innerText = jsonData.files[i];
+						select.append( option );
 					}
+
+					document.querySelector( '#wavContainer' ).append( select );
+					document.querySelector( '#currentFiles' ).className = '';
+					document.querySelector( '#clearDirectory' ).className = '';
 				}
-			} );
+			}
 		}
-	}
+	} );
 
 	document.querySelector( '#clearDirectory' ).addEventListener( 'submit', ( e ) => {
 		fetch( '/music/delete', {
