36d39b874f86540ec91eba1705f4f1a0eb394a45
Alex Pieschel <apieschel@gmail.com>
2022-08-09 11:02:32 -0500

    modified pages api to recieve post data on per page basis

--- api/pages.php
+++ api/pages.php
@@ -4,14 +4,6 @@
 	require_once( 'header.php' );
 	require_once( 'footer.php' );
 
-	if( DEBUG && DEBUG_DISPLAY ) {
-		ini_set( 'display_errors', 1 );
-		ini_set( 'display_startup_errors', 1 );
-		error_reporting( E_ALL );
-	}
-
-	// echo '<pre>'; var_dump( $_SERVER ); echo '</pre>';
-
 	if( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 		global $headString;
 		global $headerString;
@@ -25,68 +17,69 @@
 		] );
 
 		$result = curl_exec( $curl );
-
 		curl_close( $curl );
-		
-		$pages = ( array ) json_decode( $result );
 
-		if( isset( $_POST['post'] ) ) {
-			file_put_contents( ABSPATH . 'api/' . $_POST['title'] . '.json', $_POST['post'] );
-		}
-		
-		foreach( $pages as $page ) {
-			if( $page->slug !== 'gitfeed' && $page->slug !== 'twitch-api' ) {
+		if( isset( $_POST['post'] ) && curl_getinfo( $curl, CURLINFO_HTTP_CODE ) === 200 ) {
+			$oldPageData = json_decode( file_get_contents( ABSPATH . 'api/' . $_POST['slug'] . '.json' ) );
+			$expired = $pageData->timestamp <= strtotime( '-1 minute', time() );
+
+			if( $expired ) {
+				$newPageData = ( array ) json_decode( $_POST['post'] );
+				$newPageData['timestamp'] = time();
+				file_put_contents( ABSPATH . 'api/' . $_POST['slug'] . '.json', json_encode( $newPageData ) );
+				
 				$markup = $headString;
 
-				if( $page->slug === 'pomodoro' ) {
-					$markup .= '<body class="page-template-page-pomodoro">';
-				} else if( $page->slug === 'simon' ) {
-					$markup .= '<body class="page-template-page-simon">';
-				} else if( $page->slug === 'tic-tac-toe' ) {
-					$markup .= '<body class="page-template-page-tic-tac-toe">';
-				} else if( $page->slug === 'javascript-drum-machine' ) {
-					$markup .= '<body class="javascript-drum-machine">';
-				}
-				else {
-					$markup .= '<body class="page-template-default">';
+				switch( $_POST['slug'] ) {
+					case 'pomodoro':
+						$markup .= '<body class="page-template-page-pomodoro">';
+						break;
+					case 'simon':	
+						$markup .= '<body class="page-template-page-simon">';
+						break;
+					case 'tic-tac-toe':
+						$markup .= '<body class="page-template-page-tic-tac-toe">';
+						break;
+					case 'javascript-drum-machine':
+						$markup .= '<body class="javascript-drum-machine">';
+						break;
+					default:
+						$markup .= '<body class="page-template-default">';		
 				}
 
 				$markup .= $headerString;
-				$markup .= '<main><h1>' . $page->title->rendered . '</h1>';
-				$markup .= $page->content->rendered . '</main>';
+				$markup .= '<main><h1>' . $_POST['title'] . '</h1>';
+				$markup .= $newPageData['content']->rendered . '</main>';
 
-				if( $page->slug === 'javascript-calculator' ) {
-					$markup .= '<script src="assets/js/calc.js"></script>';
-				}
-
-				if( $page->slug === 'tic-tac-toe' ) {
-					$markup .= '<script src="assets/js/tictac.js"></script>';
-				}
-
-				if( $page->slug === 'simon' ) {
-					$markup .= '<script src="assets/js/simon.js"></script>';
-				}
-
-				if( $page->slug === 'pomodoro' ) {
-					$markup .= '<script src="assets/js/pomodoro.js"></script>';
-				}
-
-				if( $page->slug === 'javascript-drum-machine' ) {
-					$markup .= '
-						<script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
-						<script src="assets/js/client.js"></script>
-						<script src="assets/js/drummachine.js"></script>
-					';
+				switch( $_POST['slug'] ) {
+					case 'pomodoro':
+						$markup .= '<script src="assets/js/pomodoro.js"></script>';
+						break;
+					case 'javascript-calculator':
+						$markup .= '<script src="assets/js/calc.js"></script>';
+						break;
+					case 'simon':	
+						$markup .= '<script src="assets/js/simon.js"></script>';
+						break;
+					case 'tic-tac-toe':
+						$markup .= '<script src="assets/js/tictac.js"></script>';
+						break;
+					case 'javascript-drum-machine':
+						$markup .= '
+							<script src="https://cdn.rawgit.com/mattdiamond/Recorderjs/08e7abd9/dist/recorder.js"></script>
+							<script src="assets/js/client.js"></script>
+							<script src="assets/js/drummachine.js"></script>
+						';
+						break;
+					default:
+						$markup .= '<body class="page-template-default">';		
 				}
 				
-				$markup .= $footerString;	
-				
-				file_put_contents( ABSPATH . $page->slug . '.html', $markup );
+				$markup .= $footerString;
+				file_put_contents( ABSPATH . $_POST['slug'] . '.html', $markup );
 			}
 		}
 	} else {
 		echo "Sorry, but you don't have access to this page.";
 	}
-	
-	$pages['timestamp'] = time();
 ?>
