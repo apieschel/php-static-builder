d209177623cfca45fa857e6dc8809deb42fc8c4f
Alex Pieschel <apieschel@gmail.com>
2022-08-12 12:41:13 -0500

    serve animate css locally

--- api/head.php
+++ api/head.php
@@ -10,7 +10,7 @@
 		<link rel="icon" type="image/png" href="assets/img/favicon.png"/>
 		<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
 		<link rel="stylesheet" href="assets/lightbox/dist/css/lightbox.min.css">
-		<link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css?ver=6.0.1" type='text/css' media='all' />
+		<link rel='stylesheet' href="assets/css/animate.min.css" type='text/css' media='all' />
 		<link rel="stylesheet" href="assets/css/style.css">
 		<script src="assets/js/jquery-3.2.1.min.js"></script> 
 	</head>
