d8d247e9bf28d0ae38a7393cb2aeaa67db444ee7
Alex Pieschel <apieschel@gmail.com>
2022-08-12 10:59:29 -0500

    change portfolio featured images to webp extension

--- api/index.php
+++ api/index.php
@@ -84,7 +84,7 @@
 
 				<div class="project-container">
 					<a href="' . $project->custom_fields->{'Project URL'}[0] . '" target="_blank">
-						<img width="890" height="938" src="/assets/img/' . $project->_embedded->{'wp:featuredmedia'}[0]->media_details->sizes->full->file . '">
+						<img width="890" height="938" src="/assets/img/' . $project->_embedded->{'wp:featuredmedia'}[0]->slug . '.webp">
 					</a>
 					
 					<div class="project-tile">
