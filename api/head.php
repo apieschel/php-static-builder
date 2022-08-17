9a5deea1571aa5ce4097fd6e32b0148fcd5ec883
Alex Pieschel <apieschel@gmail.com>
2022-08-17 10:55:22 -0500

    added meta description to head, bumped font-size to be legible

--- api/head.php
+++ api/head.php
@@ -7,6 +7,7 @@
 		<meta charset="UTF-8"/>
 		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+		<meta name="description" content="Portfolio site featuring javascript projects and websites designed and built for clients. Category: Web Development">
 		<link rel="icon" type="image/png" href="assets/img/favicon.png"/>
 		<link rel="stylesheet" href="assets/css/font-awesome/css/font-awesome.min.css">
 		<link rel="stylesheet" href="assets/lightbox/dist/css/lightbox.min.css">
